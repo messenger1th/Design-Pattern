0b03a61dad860d6b494329acad28e1b60ecccc9f
Li <1832114135@qq.com>
2022-08-24 20:21:24 +0800

    add game mode readme

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <share.h>
+#include <bits/stdc++.h>
+#include <shared_mutex>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+std::mutex m;
+bool empty() {
+    std::unique_lock<std::mutex> lock(m);
+    return true;
+}
+
+bool check() {
+    std::unique_lock<std::mutex> lock(m);
+    if (empty()) return false;
+    return true;
+}
+
+class Base {
+public:
+    virtual void print() {
+
+        cout << "base " << endl;
+    }
+};
+
+class D: public Base{
+public:
+    virtual void print() {
+        Base::print();
+        cout << " d" << endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    D* d = new D;
+    d->print();
 }
